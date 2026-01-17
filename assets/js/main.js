f5f2ee70bcefe94cb329ee73bc080edba9ca87f7
Dominic Gena <dominic.gena2@gmail.com>
2026-01-16 21:04:25 -0500

    1/16/2026 @ 9:00 PM -- Made a working slideshow as the background. I want to add transitions, though. I'm a little worried about optimization and everything with loading the images, but I think it's going in a good direction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 async function initSite() {
     try {
         const response = await fetch('/assets/data/config.json');// fetch config
-        const config = await response.json();
+        const config = await response.json();// config file
 
         document.getElementById('server_title').innerText = config.server.title;// populate title and version from json
         document.getElementById('version').innerText = `Version ${config.server.version}`;
@@ -27,11 +27,41 @@ async function initSite() {
         });
 
         console.log("Sundown SMP: Navigation and config loaded successfully.");
+        slideshow(config);
 
     } catch (error) {
         console.error("Error loading site configuration:", error);
         document.getElementById('server_title').innerText = "Sundown SMP";
     }
+}
+
+async function slideshow(config){
+    const delay = 5000;
+    if(document.URL.includes("/gallery/"))return;// don't do the slideshow if in gallery, because gallery should be displayed in a grid.
+    const layerCurrent = document.getElementById('layer-current');
+    const layerNext = document.getElementById('layer-next');
+    const imgPath = config.web.gallery_path.name;
+    const imgData = config.web.gallery;
+    // console.log("imgData: ", imgData);
+    // sequence();
+
+    const sequence = () => {
+        let choiceCurrent = Math.floor(Math.random() * imgData.length);// random number between 0 and the number of images in the /assets/img/uploads/ folder
+
+        // pick a different number for the next element
+        do {
+            choiceNext = Math.floor(Math.random() * imgData.length);
+        } while (choiceNext === choiceCurrent);
+
+        console.log("Selected image index ", choiceCurrent, " as current image.");
+        console.log("Selected image index ", choiceNext, " as next image.");
+        layerCurrent.src = `${imgPath}/${imgData[choiceCurrent].name}`;
+        layerCurrent.alt = `${imgData[choiceCurrent].alt_text}`;
+        layerNext.src = `${imgPath}/${imgData[choiceNext].name}`;
+        layerNext.alt = `${imgData[choiceNext].alt_text}`;
+    };
+    sequence();// prepare with first image
+    setInterval(sequence, delay);
 }
 
 
